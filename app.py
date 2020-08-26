59912a92b6159997018b10b8a0e0077b374976c8
Elvan02-byte <elvan.listiawan02@gmail.com>
2020-08-26 09:17:15 +0700

    scrapping waktu sholat

--- app.py
+++ app.py
@@ -6,5 +6,24 @@ contents = requests.get(url)
 #print(contents.text)
 
 response = bs4.BeautifulSoup(contents.text, "html.parser")
+data = response.find_all('tr', 'table_highlight')
+data = data[0]
 
-print(response)
+sholat = {}
+i = 0
+for d in data :
+        if i == 1 :
+            sholat ['subuh'] = d.get_text()
+        elif i == 2 :
+            sholat ['duhur'] = d.get_text()
+        elif i == 3:
+            sholat['ashar'] = d.get_text()
+        elif i == 4:
+            sholat['maghrib'] = d.get_text()
+        elif i == 5:
+            sholat['isya'] = d.get_text()
+        i += 1
+
+print(sholat)
+print(sholat['ashar'])
+
